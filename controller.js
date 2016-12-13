cbd2b575652dd692397368e184d654206511c358
marcandy <marcandy.nj@gmail.com>
2016-12-13 12:06:56 -0700

    review

--- controller.js
+++ controller.js
@@ -1,5 +1,12 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
+angular.module('userProfiles').controller('MainController', function($scope, mainServ){
+
+
+
+    $scope.getUsers = function () {
+      $scope.users = mainServ.getUsers();
+      return $scope.users;
+    }
+
+    $scope.getUsers();
+    $scope.toggleFavorite = mainServ.toggleFavorite;
 })
